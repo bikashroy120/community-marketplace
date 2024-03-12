0d3afdcf84b3375018e42fa898b87ffb8b5291ba
bikashroy120 <bikashroydt@gmail.com>
2024-03-12 08:08:20 +0600

    post upload form add

--- Apps/Screen/AddPostScreen.jsx
+++ Apps/Screen/AddPostScreen.jsx
@@ -6,19 +6,28 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Alert,
+  ActivityIndicator,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 import { app } from "../../firebaseConfig";
 import { Formik } from "formik";
 import { Picker } from "@react-native-picker/picker";
 import * as ImagePicker from "expo-image-picker";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { useUser } from "@clerk/clerk-expo";
 
 const db = getFirestore(app);
 export default function AddPostScreen() {
   const [categoryList, setCategoryList] = useState([]);
   const [image, setImage] = useState(null);
-
+  const storage = getStorage();
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     getCategoryData();
   }, []);
@@ -51,124 +60,227 @@ export default function AddPostScreen() {
     }
   };
 
-  const handelSubmit = (value)=>{
-    value.image = image;
-    console.log(value)
-  }
+  const handelSubmit = async (value, { resetForm }) => {
+    setLoading(true);
+    try {
+      console.log(value);
 
+      // Upload image to Firebase Storage
+      const resp = await fetch(image);
+      const blob = await resp.blob();
+      const storageRef = ref(storage, "communityPost/" + Date.now() + ".jpg");
+      const snapshot = await uploadBytes(storageRef, blob);
 
+      // Get download URL of the uploaded image
+      const downloadUrl = await getDownloadURL(storageRef);
+
+      // Add additional form data
+      value.image = downloadUrl;
+      value.userName = user?.fullName;
+      value.userEmail = user?.primaryEmailAddress?.emailAddress;
+      value.userImage = user?.imageUrl;
+
+      // Add form data to Firestore collection
+      const docRef = await addDoc(collection(db, "UserPost"), value);
+
+      if (docRef.id) {
+        Alert.alert("Success!!!", "Post add success!");
+        setLoading(false);
+        resetForm();
+      }
+    } catch (error) {
+      console.error("Error handling form submission:", error);
+      throw error; // Re-throw the error to be caught by the caller
+      setLoading(false);
+    }
+  };
   return (
-    <View className=" mt-10 p-5">
-      <Text className=" font-bold text-[25px]">Add New Post</Text>
-      <Text className=" text-gray-500">Create New and Start Selling</Text>
-      <Formik
-        initialValues={{
-          name: "",
-          category: "",
-          desc: "",
-          address: "",
-          price: "",
-        }}
-        onSubmit={(values) => handelSubmit(values)}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          setFieldValue,
-        }) => (
-          <View>
-            <TouchableOpacity onPress={() => pickImage()} className=" my-3">
-              {image ? (
-                <Image
-                  source={{ uri: image }}
-                  style={{
-                    width: 120,
-                    height: 100,
-                    borderRadius: 15,
-                    backgroundColor: "gray",
-                  }}
-                />
-              ) : (
-                <Image
-                  source={require("../../assets/image/image-gallery.jpg")}
-                  style={{
-                    width: 120,
-                    height: 100,
-                    borderRadius: 15,
-                    backgroundColor: "gray",
-                  }}
-                />
-              )}
-            </TouchableOpacity>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={{ marginTop: 50, marginBottom: 20, paddingHorizontal: 20 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 25 }}>Add New Post</Text>
+        <Text style={{ color: "gray" }}>Create New and Start Selling</Text>
+        <Formik
+          initialValues={{
+            name: "",
+            category: "",
+            desc: "",
+            address: "",
+            price: "",
+            userName: "",
+            userEmail: "",
+            userImage: "",
+          }}
+          validate={(values) => {
+            const errors = {};
 
-            <TextInput
-              onChangeText={handleChange("name")}
-              onBlur={handleBlur("name")}
-              value={values?.name}
-              style={styles.input}
-              placeholder="Title"
-            />
-            <TextInput
-              onChangeText={handleChange("desc")}
-              onBlur={handleBlur("desc")}
-              value={values?.desc}
-              style={styles.input}
-              numberOfLines={5}
-              placeholder="Description"
-            />
-            <TextInput
-              onChangeText={handleChange("price")}
-              onBlur={handleBlur("price")}
-              value={values?.price}
-              style={styles.input}
-              placeholder="Price"
-              keyboardType="number-pad"
-            />
+            if (!values.name) {
+              errors.name = "Title is required";
+            }
 
-            <View
-              style={{
-                borderWidth: 1,
-                borderRadius: 10,
-                marginTop: 10,
-                marginBottom: 5,
-              }}
-            >
-              <Picker
-                selectedValue={values?.category}
-                onValueChange={(itemValue) =>
-                  setFieldValue("category", itemValue)
-                }
+            if (!values.category) {
+              errors.category = "Category is required";
+            }
+
+            if (!values.desc) {
+              errors.desc = "Description is required";
+            }
+
+            if (!values.price) {
+              errors.price = "Price is required";
+            } else if (isNaN(values.price)) {
+              errors.price = "Price must be a number";
+            }
+
+            if (!values.address) {
+              errors.address = "Address is required";
+            }
+
+            return errors;
+          }}
+          onSubmit={handelSubmit}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            setFieldValue,
+            errors,
+          }) => (
+            <View>
+              <TouchableOpacity
+                onPress={() => pickImage()}
+                style={{ marginVertical: 10 }}
               >
-                {categoryList &&
-                  categoryList.map((item, index) => (
-                    <Picker.Item
-                      key={index}
-                      label={item?.name}
-                      value={item?.name}
-                    />
-                  ))}
-              </Picker>
-            </View>
+                {image ? (
+                  <Image
+                    source={{ uri: image }}
+                    style={{
+                      width: 120,
+                      height: 100,
+                      borderRadius: 15,
+                      backgroundColor: "gray",
+                    }}
+                  />
+                ) : (
+                  <Image
+                    source={require("../../assets/image/image-gallery.jpg")}
+                    style={{
+                      width: 120,
+                      height: 100,
+                      borderRadius: 15,
+                      backgroundColor: "gray",
+                    }}
+                  />
+                )}
+              </TouchableOpacity>
 
-            <TextInput
-              onChangeText={handleChange("address")}
-              onBlur={handleBlur("address")}
-              value={values?.address}
-              style={styles.input}
-              placeholder="Address"
-            />
-            {/* <Button onPress={handleSubmit} title="Submit" /> */}
-            <TouchableOpacity className="py-3 px-5 bg-blue-500 rounded-[10px] mt-5">
-              <Text className=" text-white text-center font-semibold text-[18px]">
-                Submit
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Formik>
-    </View>
+              <View style={{ marginBottom: 10 }}>
+                <TextInput
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                  value={values?.name}
+                  style={styles.input}
+                  placeholder="Title"
+                />
+                {errors.name && (
+                  <Text style={styles.error} className="text-red-500">
+                    {errors.name}
+                  </Text>
+                )}
+              </View>
+              <View style={{ marginBottom: 10 }}>
+                <TextInput
+                  onChangeText={handleChange("desc")}
+                  onBlur={handleBlur("desc")}
+                  value={values?.desc}
+                  style={styles.input}
+                  numberOfLines={5}
+                  placeholder="Description"
+                />
+                {errors.desc && (
+                  <Text style={styles.error} className="text-red-500">
+                    {errors.desc}
+                  </Text>
+                )}
+              </View>
+              <View style={{ marginBottom: 10 }}>
+                <TextInput
+                  onChangeText={handleChange("price")}
+                  onBlur={handleBlur("price")}
+                  value={values?.price}
+                  style={styles.input}
+                  placeholder="Price"
+                  keyboardType="number-pad"
+                />
+                {errors.price && (
+                  <Text style={styles.error} className="text-red-500">
+                    {errors.price}
+                  </Text>
+                )}
+              </View>
+
+              <View
+                style={{ borderWidth: 1, borderRadius: 10, marginBottom: 10 }}
+              >
+                <Picker
+                  selectedValue={values?.category}
+                  onValueChange={(itemValue) =>
+                    setFieldValue("category", itemValue)
+                  }
+                >
+                  {categoryList &&
+                    categoryList.map((item, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={item?.name}
+                        value={item?.name}
+                      />
+                    ))}
+                </Picker>
+              </View>
+              <View style={{ marginBottom: 10 }}>
+                <TextInput
+                  onChangeText={handleChange("address")}
+                  onBlur={handleBlur("address")}
+                  value={values?.address}
+                  style={styles.input}
+                  placeholder="Address"
+                />
+                {errors.address && (
+                  <Text style={styles.error} className="text-red-500">
+                    {errors.address}
+                  </Text>
+                )}
+              </View>
+              <TouchableOpacity
+                onPress={() => handleSubmit()}
+                disabled={loading}
+                style={{
+                  backgroundColor: loading ? "#ccc":"blue",
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                {loading ? (
+                  <ActivityIndicator color={"#fff"} />
+                ) : (
+                  <Text
+                    style={{
+                      color: "white",
+                      textAlign: "center",
+                      fontSize: 18,
+                    }}
+                  >
+                    Submit
+                  </Text>
+                )}
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -183,5 +295,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 5,
     textAlignVertical: "top",
+  },
+  error: {
+    color: "red",
   },
 });
